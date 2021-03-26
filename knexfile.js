0c8620f8e888cc145f9121d7bd3272185924f9aa
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-03-26 00:27:17 -0600

    please help

--- knexfile.js
+++ knexfile.js
@@ -22,13 +22,12 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: true,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: 'quotes',
       directory: './migrations'
     }
   }
