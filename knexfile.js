b7f156e8480cf58e43024c264ae8510aa5e4e757
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-04-22 22:58:17 -0600

    fixing ssl again

--- knexfile.js
+++ knexfile.js
@@ -24,12 +24,7 @@ module.exports = {
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-        ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
-        key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-        cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
-      },
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
