8c20028505d182cb9cfc1188c80a426b5b40aabd
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-09-01 13:26:49 -0600

    trying things

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const dbEngine = process.env.DB_ENVIRONMENT || "development";
 import knex from "knex";
-import knexConfig from "./knexfile.js";
-knexConfig[dbEngine]
+import knexConfig from "./knexfile.js"
+knexConfig = knexConfig[dbEngine];
 const db = knex(knexConfig);
 
 export default db;
