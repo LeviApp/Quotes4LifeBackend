75a81177e41cc0eb1c6a5f84cd330b5beb7b0dce
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-09-01 12:28:11 -0600

    added value to dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,3 +1,5 @@
 const dbEngine = process.env.DB_ENVIRONMENT || "development";
 const knexConfig = require("./knexfile.js")[dbEngine];
-module.exports = require("knex")(knexConfig);
+const db = require("knex")(knexConfig);
+
+module.exports = db;
